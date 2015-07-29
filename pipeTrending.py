31f8fa304df70cf655dd62b1ae1a52ee0bde9cce
Jun Liu <antyser@gmail.com>
2015-07-29 11:32:40 -0700

    for trending

--- pipeTrending.py
+++ pipeTrending.py
@@ -4,10 +4,8 @@ from kafka import SimpleProducer, KafkaClient, SimpleConsumer
 from kafka.common import MessageSizeTooLargeError
 import json,logging, time
 
-#cpp.pages -> process
-
 IN_KAFKA_HOST = '172.31.20.238:9092'
-CONSUMER_TOPIC = 'cpp.pages'
+CONSUMER_TOPIC = 'schedule'
 CONSUMER_GROUP = 'trending'
 OUT_KAFKA_HOST = '172.31.20.238:9092'
 PRODUCER_TOPIC='process'
@@ -20,15 +18,16 @@ def fetchFrom():
 
     for msg in consumer:
         record = json.loads(msg.message.value)
-        if '_trends' in record['metadata']:
+        if 'tags' in record and '_trends' in record['tags']:
             try:
                 producer.send_messages("trends", msg.message.value)
                 print(str(time.strftime("%c")) + " pump url " + record['inlink'].encode('utf-8'))
             except MessageSizeTooLargeError as err:
                 logging.warning(err)
             continue
-
-        if '_channel' in record['metadata']:
+        if 'metadata' in record:
+            print record['metadata']
+        if 'metadata' in record and 'tags' in record['metadata'] and '_channels' in record['metadata']['tags']:
             try:
                 producer.send_messages("channels", msg.message.value)
                 print(str(time.strftime("%c")) + " pump url " + record['inlink'].encode('utf-8'))
---
+++ trendingReport.py
@@ -0,0 +1,46 @@
+import datetime
+from kafka import KafkaClient, SimpleConsumer
+import json,logging, time, os, sys
+
+#cpp.pages -> process
+
+IN_KAFKA_HOST = '172.31.20.238:9092'
+CONSUMER_TOPIC = 'trends'
+CONSUMER_GROUP = 'trends_report_test'
+
+
+def dumpTsv(query, filename):
+    with open(filename, "w+") as file:
+        for key in query:
+            for line in query[key]:
+                file.write('\t'.join(key) + "\t")
+                file.write(line + "\n")
+
+
+
+
+def fetchFrom():
+    in_kafka = KafkaClient(IN_KAFKA_HOST)
+    consumer = SimpleConsumer(in_kafka, CONSUMER_GROUP, CONSUMER_TOPIC, max_buffer_size=20*1024*1024)
+    query = {}
+    while True:
+        current = int(time.time()) - 100
+        for msg in consumer:
+            try:
+                record = json.loads(msg.message.value)
+                stream = record["stream"]
+                rank = str(record["metadata"]["trending_rank"])
+                source = record["metadata"]["trending_source"]
+                word = record["metadata"]["trending_word"]
+                links = []
+                for link in record["links"]:
+                    links.append(word + '\t' + str(link["metadata"]["linkrank"]) + '\t' + link["url"])
+                t = (stream, source, rank)
+                query[t] = links
+                if current < record["timestamp"]:
+                    filename = datetime.datetime.fromtimestamp(current).strftime('%Y-%m-%d_%H:%M:%S')
+                    dumpTsv(query, os.path.join(sys.argv[1],filename))
+                    break
+            except Exception as e:
+                logging.warning(e)
+        time.sleep(100)
